4f2d7b463cb56132a49ce5d1a81c57971c45c763
Felipe Costa <costa.felipe52@gmail.com>
2020-02-16 15:28:17 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_res/ex_res_06/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_06/main.c
@@ -10,6 +10,51 @@ Data:
 
 int main(int argc, char const *argv[])
 {
+	int a[4][5], b[5][2], c[4][2];
+	int i, j, k, soma, mult;
+
+	// preenche o primeiro vetor
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			printf("\n[%d][%d]: ", i+1, j+1);
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	// preenche o segundo vetor
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("\n[%d][%d]: ", i+1, j+1);
+			scanf("%d", &b[i][j]);			
+		}
+	}
 	
+	soma = 0;
+	for (i = 0; i < 4; i++)
+	{
+		for (k = 0; k < 2; k++)
+		{
+			for (j = 0; j < 5; j++)
+			{
+				mult = a[i][j] * b[j][k];
+				soma += mult;
+			}
+			c[i][k] = soma;
+			soma = 0;
+		}
+	}
+	
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("\n[%d][%d]= %d", i, j, c[i][j]);
+		}
+	}
+		
 	return 0;
 }
--- Lista07_Felipe_deAssis/ex_res/ex_res_07/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_07/main.c
@@ -8,8 +8,62 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 5
+#define tam2 7
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], menor, maior, linha, coluna, cont;
+	int i, j;
+
+	cont = 0;
+
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+	
+	for (i = 0; i < tam1; i++)
+	{
+		menor = matriz[i][j];
+		linha = i;
+		coluna = 0;
+		for (j = 0; j < tam2; j++)
+		{
+			if (matriz[i][j] < menor)
+			{
+				menor = matriz[i][j];
+				linha = i;
+				coluna = j;
+			}
+		}
+		maior = matriz[0][coluna];
+		for (j = 0; j < tam2; j++)
+		{
+			if (matriz[j][coluna])
+			{
+				maior = matriz[j][coluna];
+			}
+		}
+		if (menor == maior)
+		{
+			printf("\nPonto de sela: %d, na posição [%d][%d]:", maior, linha, coluna);
+			cont++;
+		}
+	}	
+	
+	if (cont == 0)
+	{
+		printf("\nERRO! Não existe ponto de sela nesta matriz.");
+	}
 	
 	return 0;
 }
+
+// Um “ponto de sela” é caracterizado por uma entrada em uma matriz, 
+// tal que esta seja o valor mínimo (menor valor) para sua linha e o
+// máximo (maior valor) para sua coluna. 
