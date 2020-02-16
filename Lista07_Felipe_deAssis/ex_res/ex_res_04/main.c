1562f3f4f8c1a14b1c7d3ba15d03cd79fd5ec697
Felipe Costa <costa.felipe52@gmail.com>
2020-02-16 13:18:41 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_res/ex_res_04/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_04/main.c
@@ -8,8 +8,52 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 2 //10
+#define tam2 5 //20
+
 int main(int argc, char const *argv[])
 {
-	
+	int matriz[tam1][tam2], soma[tam1];
+	int i, j;
+
+	// recebe o valor das matrizes 
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\nLinha %d - Coluna %d: ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+
+	// realiza a soma das linhas das matrizes 
+	for (i = 0; i < tam1; i++)
+	{
+		soma[i] = 0;
+		for (j = 0; j < tam2; j++)
+		{
+			soma[i] += matriz[i][j];
+		}
+	}
+
+	// multiplica cada valor da linha pela soma da linha 
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			matriz[i][j] *= soma[i];
+		}
+	}
+
+	// exibe o resultado final 
+	for (i = 0; i < tam1; i++)
+	{
+		printf("\nLinha %d: ", i+1);
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\nMatriz [%d][%d] = %d", i+1, i+j, matriz[i][j]);
+		}
+	}	
+		
 	return 0;
 }
