2b44dd8930cd6fddddfc91dfa46d54015c9a7d35
Felipe Costa <costa.felipe52@gmail.com>
2020-02-16 13:53:40 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_res/ex_res_05/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_05/main.c
@@ -8,8 +8,52 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 2 //4
+#define tam2 4 //7
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], maior, menor, menorDaLinha, coluna;
+	int i, j;
 	
+	// recebe os valores 
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]: ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+	
+	// calcula o menor 
+	menor = matriz[0][0];
+	menorDaLinha = 0;
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			if (matriz[i][j] < menor)
+			{
+				menor = matriz[i][j];
+				menorDaLinha = i;
+			}
+		}
+	}
+	
+	// calcula o maior 
+	maior = matriz[menorDaLinha][0];
+	coluna = 0;
+	for (j = 0; j < tam2; j++)
+	{
+		if (matriz[menorDaLinha][j] > maior)
+		{
+			maior = matriz[menorDaLinha][j];
+			coluna = j;
+		}
+	}
+
+	printf("\nMINMAX\t[%d][%d]: %d", menorDaLinha, coluna, maior);
+
 	return 0;
 }
