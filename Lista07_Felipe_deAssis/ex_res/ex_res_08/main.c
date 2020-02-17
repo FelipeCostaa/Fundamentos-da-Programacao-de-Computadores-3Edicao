a8b632e7c29d10ae5bf2981a7b3a80c103193b0f
Felipe Costa <costa.felipe52@gmail.com>
2020-02-17 08:37:30 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_res/ex_res_08/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_08/main.c
@@ -8,8 +8,58 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 6
+#define tam2 4
+
 int main(int argc, char const *argv[])
 {
+	int matriz1[tam1][tam2], maiorQue30 = 0, matriz2[tam1][tam2];
+	int i, j;
+
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz1[i][j]);
+		}
+	}
+
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			matriz2[i][j] = matriz1[i][j];
+			
+			if (matriz1[i][j] > 30)
+			{
+				maiorQue30++;
+			}
+			if (matriz1[i][j] == 30)
+			{
+				matriz2[i][j] = 0;
+			}
+		}
+	}
+	system("clear");
+	
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz2[i][j]);
+		}
+	}
+	
+	
+	if (maiorQue30 == 0)
+	{
+		printf("\nEsse vetor não possui numeros maiores que 30.");
+	}
+	else
+	{
+		printf("\nQuantidade de numeros maiores que 30: %d", maiorQue30);
+	}	
 	
 	return 0;
 }
--- Lista07_Felipe_deAssis/ex_res/ex_res_09/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_09/main.c
@@ -8,8 +8,103 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 15
+#define tam2 5
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], repetidos[tam1][tam2], vezes[tam1][tam2];
+	int linha1, linha2, coluna, x, num, quant, achou;
+	int i, j, k, l;
+
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+	
+	linha1 = 0;
+	coluna = 0;
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam2; j++)
+		{
+			quant = 0;
+			num = matriz[i][j];
+
+			for (k = 0; k < tam1; k++)
+			{
+				for (l = 0; l < tam2; l++)
+				{
+					if (matriz[i][j] == num)
+					{
+						quant++;
+					}
+				}
+				if (quant > 1)
+				{
+					achou = 0;
+					if (coluna == 1)
+					{
+						linha2 = linha1 - 1;
+					}
+					else
+					{
+						linha2 = linha1;
+					}
+					for (k = 0; k < linha2; k++)
+					{
+						if (k < linha2)
+						{
+							x = 5;
+						}
+						else
+						{
+							x = coluna - 1;
+						}
+						for (k = 0; k < x; k++)
+						{
+							if (num == repetidos[k][l])
+							{
+								achou = 1;
+							}
+						}
+						if (achou == 0)
+						{
+							repetidos[linha1][coluna] = num;
+							vezes[linha1][coluna] = quant;
+							coluna++;
+							if (coluna > 5)
+							{
+								linha1++;
+								coluna = 1;
+							}
+						}
+					}
+				}
+			}
+			
+			for (i = 0; i < linha1; i++)
+			{
+				if (i < linha1)
+				{
+					x = 5;
+				}
+				else
+				{
+					x = coluna - 1;
+				}
+				for (j = 0; j < x; j++)
+				{
+					printf("\nO numero %d repetiu %d.", repetidos[i][j], vezes[i][j]);
+				}
+			}
+		}
+	}
+	
 	
 	return 0;
 }
