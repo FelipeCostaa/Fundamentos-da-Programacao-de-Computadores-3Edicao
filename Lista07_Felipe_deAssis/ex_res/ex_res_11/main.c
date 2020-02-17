7f8df6e420a90f6ab156ec578c73e828e4d50d8e
Felipe Costa <costa.felipe52@gmail.com>
2020-02-17 09:28:20 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_res/ex_res_11/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_11/main.c
@@ -8,8 +8,44 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam 8
+
 int main(int argc, char const *argv[])
 {
-	
+	int matriz[tam][tam], simetria = 1;
+	int i, j;
+
+	// preenche uma matriz 8x8
+	for (i = 0; i < tam; i++)
+	{
+		for (j = 0; j < tam; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+
+	// verifica se a matriz é simetrica
+	for (i = 0; i < tam; i++)
+	{
+		for (j = 0; j < tam; j++)
+		{
+			if (matriz[i][j] != matriz[j][i])
+			{
+				simetria = 0;
+			}
+		}
+	}
+
+	// exibe mensagem final
+	if (simetria == 1)
+	{
+		printf("\nA matriz informada é simetrica.");
+	}
+	else
+	{
+		printf("\nA matriz informada não é simetrica.");
+	}
+			
 	return 0;
 }
