6d6a9c3b82656f741e79015e6f646d9566b1ce1b
Felipe Costa <costa.felipe52@gmail.com>
2020-02-19 18:20:40 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_res/ex_res_14/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_14/main.c
@@ -8,8 +8,80 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 3
+#define tam2 64
+
 int main(int argc, char const *argv[])
 {
+	char cidadeOrigem[tam1][tam2], cidadeDestino[tam1][tam2];
+	float distancia[tam1][tam1], consumo[tam1], quant, cont = 0;
+	int i, j;
+
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam1; j++)
+		{
+			printf("\n%dª Viagem: ", j+1);
+
+			printf("\nCidade Origem: ");
+			setbuf(stdin, NULL);
+			gets(cidadeOrigem[j]);
+
+			printf("\nCidade Destino: ");
+			setbuf(stdin, NULL);
+			gets(cidadeDestino[j]);	
+
+			if (i == j)
+			{
+				distancia[i][j] = 0;
+				printf("\nDistancia entre as cidades: 0\n");
+			}
+			else
+			{
+				printf("\nDistancia entre as cidades(em km): ");
+				scanf("%f", &distancia[i][j]);	
+			}
+			
+			printf("\nConsumo do veiculo(km/L): ");
+			scanf("%f", &consumo[i]);
+
+			// system("clear");
+			printf("\n--------------------------------------");
+		}
+		
+	}
+
+	printf("\nPercursos que não ultrapassam 250km: ");
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam1; j++)
+		{
+			if (distancia[i][j] < 250 && distancia > 0)
+			{
+				printf("\n\tOrigem: %s \tDestino: %s", cidadeOrigem[j], cidadeDestino[j]);
+				cont++;
+			}
+			
+		}
+		
+	}
+	if (cont != 0)
+	{
+		printf("\nTodas as cidades têm mais que 250km de distancia.");
+	}
+
+	printf("\n\n");
+	printf("\nTodos os percursos\t\tQuantidade de Combustivel necessaria para percorre-los");
+	for (i = 0; i < tam1; i++)
+	{
+		for (j = 0; j < tam1; j++)
+		{
+			printf("\n%dº: ", j+1);
+			printf("\n%s\ta\t%s\t|\t%.2f", cidadeOrigem[j], cidadeDestino[j], distancia[i][j]/consumo[i]);
+		}
+		
+	}
 	
+
 	return 0;
 }
--- Lista07_Felipe_deAssis/ex_res/ex_res_15/main.c
+++ Lista07_Felipe_deAssis/ex_res/ex_res_15/main.c
@@ -8,8 +8,111 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 5
+#define tam2 10
+#define tam3 4
+#define tam4 3
+
 int main(int argc, char const *argv[])
 {
+	int vetor1[tam1], vetor2[tam2], matriz[tam3][tam4], maior, menor;
+	int mult, matrizResultante[tam3][tam4], somaPares[tam3], quant[tam4];
+
+	printf("\nPrimeiro vetor: ");
+	maior = 0;
+	for (int i = 0; i < tam1; i++)
+	{
+		printf("\n%dº numero: ", i+1);
+		scanf("%d", &vetor1[i]);
+		if (vetor1[i] > maior)
+		{
+			maior = vetor1[i];
+		}
+		
+	}
+	system("clear");
 	
+	printf("\nSegundo vetor: ");
+	menor = 99999999;
+	for (int i = 0; i < tam2; i++)
+	{
+		printf("\n%dº numero: ", i+1);
+		scanf("%d", &vetor2[i]);
+		if (vetor2[i] < menor)
+		{
+			menor = vetor2[i];
+		}
+		
+	}	
+	system("clear");
+
+	mult = menor * maior;
+
+	printf("\nMatriz: ");
+	for (int i = 0; i < tam3; i++)
+	{
+		for (int j = 0; j < tam4; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+
+			// monta a matriz resultante
+			matrizResultante[i][j] = matriz[i][j] + mult;
+		}
+		
+	}
+	system("clear");
+
+	printf("\nMaior elemento do primeiro vetor: %d", maior);
+	printf("\n---------------------------------------------");
+	printf("\nMenor elemento do segundo vetor: %d", menor);
+	printf("\n---------------------------------------------");
+
+	printf("\nMatriz original: ");
+	for (int i = 0; i < tam3; i++)
+	{
+		for (int j = 0; j < tam4; j++)
+		{
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+		}
+		
+	}
+	printf("\n---------------------------------------------");
+
+	printf("\nMatriz resultante: ");
+	for (int i = 0; i < tam3; i++)
+	{
+		somaPares[i] = 0;
+		for (int j = 0; j < tam4; j++)
+		{
+			printf("\n[%d][%d]= %d", i+1, j+1, matrizResultante[i][j]);
+			if (matrizResultante[i][j] % 2 == 0)
+			{
+				somaPares[i] += matrizResultante[i][j];
+			}
+			if (matrizResultante[i][j] >= 1 && matrizResultante[i][j] <= 5)
+			{
+				quant[j]++;
+			}
+			
+		}
+		
+	}
+	printf("\n---------------------------------------------");
+
+	printf("\nSoma dos numeros pares do vetor resultante: ");
+	for (int i = 0; i < tam3; i++)
+	{
+		printf("\n%dª linha: %d", i+1, somaPares[i]);
+	}
+	printf("\n---------------------------------------------");
+
+	printf("\nQuantidade de numeros entre 1 e 5 em cada coluna da matriz resultante");
+	for (int i = 0; i < tam3; i++)
+	{
+		printf("\n%dª coluna: %d", i+1, quant[i]);
+	}
+			
+
 	return 0;
 }
