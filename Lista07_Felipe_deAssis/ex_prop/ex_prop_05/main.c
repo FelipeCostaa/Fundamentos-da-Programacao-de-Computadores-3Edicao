b67ba5bebd5f468e65f2b8e8159f8d9fa5ba34e1
Felipe Costa <costa.felipe52@gmail.com>
2020-03-01 21:33:31 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_prop/ex_prop_05/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_05/main.c
@@ -4,12 +4,49 @@ RA: 0049539
 Data:
 */
 
+// tem que ser corrigido alguns detalhes
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define mes 6
+#define semana 2
+
 int main(int argc, char const *argv[])
 {
+	float valoresVendas[mes][semana], totalMes[mes], totalSemana[semana];
+	int cont = 0;
+
+	for (int i = 0; i < mes; i++)
+	{
+		totalMes[i] = 0;
+
+		printf("\n%dº mês: ", i+1);
+		for (int j = 0; j < semana; j++)
+		{
+			printf("\n%dª semana: ", j+1);
+			printf("\nValor das vendas: R$");
+			scanf("%f", &valoresVendas[i][j]);
+
+			totalMes[i] += valoresVendas[i][j];
+			totalSemana[cont] += valoresVendas[i][i];
+			cont++; 
+		}
+		printf("\n--------------------------");
+	}
+	
+	for (int i = 0; i < mes; i++)
+	{
+		printf("\nO total vendido no %dº mês foi de: R$%.2f", i+1, totalMes[i]);
+	}
+	printf("\n--------------------------");
+	
+	for (int i = 0; i < cont; i++)
+	{
+		printf("\nTotal vendido na %dª semana: R$%.2f", i+1, totalSemana[i]);
+	}
+	
 	
 	return 0;
 }
