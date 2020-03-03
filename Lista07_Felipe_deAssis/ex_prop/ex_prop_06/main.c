805404493977117d71430d66ebf7af9b0bd6feee
Felipe Costa <costa.felipe52@gmail.com>
2020-03-03 18:15:01 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_prop/ex_prop_06/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_06/main.c
@@ -8,8 +8,60 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 3
+#define tam2 3
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], somaColuna[tam2];
+
+	for (int i = 0; i < tam1; i++)
+	{
+		for (int j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+		}
+		
+	}
+	system("clear");
+
+	// soma a coluna 
+	for (int i = 0; i < tam1; i++)
+	{
+		somaColuna[i] = 0;
+		for (int j = 0; j < tam2; j++)
+		{			
+			somaColuna[i] += matriz[j][i];
+		}
+		
+	}
+
+	// // exibe a soma das colunas
+	// for (int i = 0; i < tam2; i++)
+	// {
+	// 	printf("\nSoma da %dª coluna: ", i+1);
+	// 	printf("%d\t", somaColuna[i]);	
+	// }
+	
+	for (int i = 0; i < tam1; i++)
+	{
+		for (int j = 0; j < tam2; j++)
+		{
+			matriz[i][j] *= somaColuna[i];
+		}
+		
+	}
+
+	printf("\nMatriz resultante: ");
+	for (int i = 0; i < tam1; i++)
+	{
+		for (int j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+		}
+		
+	}
 	
 	return 0;
 }
