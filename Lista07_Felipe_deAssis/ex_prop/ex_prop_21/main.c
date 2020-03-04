50bd28aa4994d7681b387e45bf70476822950920
Felipe Costa <costa.felipe52@gmail.com>
2020-03-04 13:57:11 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_prop/ex_prop_21/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_21/main.c
@@ -8,8 +8,41 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam 5
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam][tam], diagonalPrincipal[tam];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < tam; i++)
+	{
+		for (int j = 0; j < tam; j++)
+		{
+			matriz[i][j] = (rand() % 100) + 1;
+
+			if (i == j)
+			{
+				diagonalPrincipal[i] = matriz[i][j];
+			}
+			
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+		}
+		
+	}
+	
+	printf("\n\nNova matriz: ");
+	for (int i = 0; i < tam; i++)
+	{
+		for (int j = 0; j < tam; j++)
+		{
+			matriz[i][j] *= diagonalPrincipal[i];
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+		}
+		
+	}
+	
 	
 	return 0;
 }
--- Lista07_Felipe_deAssis/ex_prop/ex_prop_22/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_22/main.c
@@ -8,8 +8,40 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 6
+#define tam2 10
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], somaColunas[tam2], cont = 0;
+
+	srand(time(NULL));
+
+	printf("\nMatriz inicial: ");
+	for (int i = 0; i < tam1; i++)
+	{
+		somaColunas[i] = 0;
+		cont = 0;
+		for (int j = 0; j < tam2; j++)
+		{
+			matriz[i][j] = (rand() % 100) + 1;
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+
+			somaColunas[j] += matriz[cont][j];
+			cont++;
+		}
+		
+	}
+	
+	printf("\nSomas das colunas: ");
+	for (int i = 0; i < tam2; i++)
+	{
+		printf("\n%dª coluna: %d", i+1, somaColunas[i]);
+	}
+	
 	
 	return 0;
 }
+
+// Crie um programa que preencha uma matriz 6  10, some as colunas individualmente e acumule as so-
+// mas na 7 a linha da matriz. O programa deverá mostrar o resultado de cada coluna.
