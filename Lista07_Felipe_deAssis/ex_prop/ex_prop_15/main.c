46919aee904fc5ac1248d9911833e50ccce2ff25
Felipe Costa <costa.felipe52@gmail.com>
2020-03-04 10:52:06 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_prop/ex_prop_15/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_15/main.c
@@ -8,8 +8,48 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 12
+#define tam2 13
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], maiorLinha[tam1], i, j;
+
+	srand(time(NULL));
+
+	printf("\nMatriz Inicial: ");
+	for (int i = 0; i < tam1; i++)
+	{ 
+		maiorLinha[i] = matriz[i][j];
+		for (int j = 0; j < tam2; j++)
+		{
+			do
+			{
+				matriz[i][j] = (rand() % 100) + 1;
+			} while (matriz[i][j] <= 0);
+			
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+
+			if (matriz[i][j] > maiorLinha[i])
+			{
+				maiorLinha[i] = matriz[i][j];
+			}
+			
+		}
+		
+	}
 	
+	printf("\nMatriz gerada atraves dos valores iniciais divididos pelo maior elemento da linha: ");
+	// for (int i = 0; i < tam1; i++)
+	// {
+	// 	for (int j = 0; j < tam2; j++)
+	// 	{
+	// 		matriz[i][j] = matriz[i][j] / maiorLinha[i];
+	// 		printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+	// 	}
+		
+	// }
+	
+
 	return 0;
 }
--- Lista07_Felipe_deAssis/ex_prop/ex_prop_16/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_16/main.c
@@ -8,8 +8,46 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam 5
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam][tam], somaLinhas[tam], somaColunas[tam];
+
+	srand(time(NULL));
+	
+	printf("\nMatriz Inicial: ");
+	for (int i = 0; i < tam; i++)
+	{ 
+		somaLinhas[i] = 0;
+		somaColunas[i] = 0;
+		
+		for (int j = 0; j < tam; j++)
+		{
+			do
+			{
+				matriz[i][j] = (rand() % 100) + 1;
+			} while (matriz[i][j] <= 0);
+			
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+
+			somaLinhas[i] += matriz[i][j];
+
+			somaColunas[j] += matriz[j][j];
+		}
+		
+	}
+	printf("\n\nVetor com a soma das linhas: ");
+	for (int i = 0; i < tam; i++)
+	{
+		printf("\n%dª linha: %d", i+1, somaLinhas[i]);
+	}
+
+	printf("\n\nVetor com a soma das colunas: ");
+	for (int j = 0; j < tam; j++)
+	{
+		printf("\n%dª coluna: %d", j+1, somaColunas[j]);
+	}
 	
 	return 0;
 }
