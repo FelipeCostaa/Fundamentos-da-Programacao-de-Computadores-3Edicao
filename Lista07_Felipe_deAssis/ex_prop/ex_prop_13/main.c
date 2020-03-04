ef717c0042deecc5548582e5b1be7c49051ccb80
Felipe Costa <costa.felipe52@gmail.com>
2020-03-04 09:31:57 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_prop/ex_prop_13/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_13/main.c
@@ -8,8 +8,41 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 6
+#define tam2 4
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam1][tam2], maiorLinha[tam1], novaMatriz[tam1][tam2];
+
+
+	for (int i = 0; i < tam1; i++)
+	{
+		maiorLinha[i] = 0;
+		for (int j = 0; j < tam2; j++)
+		{
+			printf("\n[%d][%d]= ", i+1, j+1);
+			scanf("%d", &matriz[i][j]);
+			if (matriz[i][j] > maiorLinha[i])
+			{
+				maiorLinha[i] = matriz[i][j];
+			}
+			
+		}
+		
+	}
 	
+	printf("\nMatriz resutante: ");
+	for (int i = 0; i < tam1; i++)
+	{
+		for (int j = 0; j < tam2; j++)
+		{
+			novaMatriz[i][j] = maiorLinha[i] * matriz[i][j];	
+			printf("\n[%d][%d]= %d", i+1, j+1, novaMatriz[i][j]);
+		}
+		
+	}
+	
+
 	return 0;
 }
--- Lista07_Felipe_deAssis/ex_prop/ex_prop_14/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_14/main.c
@@ -8,8 +8,44 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define linha 2
+#define coluna 3
+
 int main(int argc, char const *argv[])
 {
+	int matriz[linha][coluna];
+
+	for (int i = 0; i < linha; i++)
+	{
+		for (int j = 0; j < coluna; j++)
+		{
+			do
+			{
+				matriz[i][j] = (rand() % 100) + 1;
+			} while (matriz[i][j] <= 0);
+			 
+		}
+		
+	}
+	
+	printf("\nValores que não estão no intervalo [5, 15]: ");
+	for (int i = 0; i < linha; i++)
+	{
+		for (int j = 0; j < coluna; j++)
+		{
+			if (matriz[i][j] < 5)
+			{
+				printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+			}
+			if (matriz[i][j] > 15)
+			{
+				printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+			}
+			
+		}
+		
+	}
+	
 	
 	return 0;
 }
