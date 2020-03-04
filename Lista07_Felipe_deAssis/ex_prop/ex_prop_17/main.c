345f857a0860e11059ef12c4968d01b72576bbcb
Felipe Costa <costa.felipe52@gmail.com>
2020-03-04 11:07:47 -0300

    Exercicios da lista de algoritmos em C

--- Lista07_Felipe_deAssis/ex_prop/ex_prop_17/main.c
+++ Lista07_Felipe_deAssis/ex_prop/ex_prop_17/main.c
@@ -8,8 +8,37 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam 10
+
 int main(int argc, char const *argv[])
 {
+	int matriz[tam][tam], cont = 0;
+	float media;
+
+	srand(time(NULL));
+
+	printf("\nMatriz inicial: ");
+	for (int i = 0; i < tam; i++)
+	{
+		for (int j = 0; j < tam; j++)
+		{
+			do
+			{
+				matriz[i][j] = (rand() % 100) + 1;
+			} while (matriz[i][j] <= 0);
+			printf("\n[%d][%d]= %d", i+1, j+1, matriz[i][j]);
+
+			if (i == j)
+			{
+				media += matriz[i][j];
+				cont++;
+			}
+			
+		}
+		
+	}
+	media /= cont;
+	printf("\nA media dos elementos da diagonal principal é: %.2f", media);
 	
 	return 0;
 }
