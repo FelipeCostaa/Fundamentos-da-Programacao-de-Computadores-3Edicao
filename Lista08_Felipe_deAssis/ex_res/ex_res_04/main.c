448a77be6a215ee1944aaa5a120a33c4ab86286b
Felipe Costa <costa.felipe52@gmail.com>
2020-03-06 19:18:44 -0300

    Exercicios da lista de algoritmos em C

--- Lista08_Felipe_deAssis/ex_res/ex_res_04/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_04/main.c
@@ -8,8 +8,29 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+int converterSegundosParaHoras(int seg);
+
 int main(int argc, char const *argv[])
 {
+	int seg;
+
+	printf("\nInforme a quantidade de segundos: ");
+	scanf("%d", &seg);
+
+	converterSegundosParaHoras(seg);
 	
 	return 0;
+}
+
+int converterSegundosParaHoras(int seg)
+{
+	int horas, minutos, segundos, resto;
+
+	horas = seg / 3600;
+	resto = segundos % 3600;
+	minutos = resto / 60;
+	segundos = resto % 60;
+
+	printf("\n%d:%d:%d", horas, minutos, segundos);
+
 }
