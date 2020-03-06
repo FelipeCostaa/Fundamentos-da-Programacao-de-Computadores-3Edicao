e8954d74648a6da685a0fee4c5003dcbd7e537f6
Felipe Costa <costa.felipe52@gmail.com>
2020-03-06 19:36:36 -0300

    Exercicios da lista de algoritmos em C

--- Lista08_Felipe_deAssis/ex_res/ex_res_05/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_05/main.c
@@ -8,8 +8,27 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+float percentualDeAcrescimo(float valorAntigo, float valorAtual);
+
 int main(int argc, char const *argv[])
 {
+	float valorAntigo, valorAtual;
+
+	printf("\nValor antigo do produto: R$");
+	scanf("%f", &valorAntigo);
+	printf("\nValor atual do produto: R$");
+	scanf("%f", &valorAtual);
+
+	printf("\nO percentual de acrescimo entre os valores é de: %.2f%%", percentualDeAcrescimo(valorAntigo, valorAtual));
 	
 	return 0;
+}
+
+float percentualDeAcrescimo(float valorAntigo, float valorAtual)
+{
+	float resultado;
+
+	resultado = ((valorAtual - valorAntigo) / valorAntigo) * 100;
+
+	return resultado;
 }
