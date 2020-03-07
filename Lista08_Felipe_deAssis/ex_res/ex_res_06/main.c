4a3b5c598de10b72e49a564b1d2ad65be29ebc0b
Felipe Costa <costa.felipe52@gmail.com>
2020-03-07 14:55:11 -0300

    Exercicios da lista de algoritmos em C

--- Lista08_Felipe_deAssis/ex_res/ex_res_06/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_06/main.c
@@ -8,9 +8,32 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+void multiplicacao(int num);
+
 int main(int argc, char const *argv[])
 {
+	int num;
+
+	do
+	{
+		printf("\nInforme um numero inteiro entre 1 e 9: \n-> ");
+		scanf("%d", &num);
+	} while (num >= 1 && num <= 9);
 	
-	
+	multiplicacao(num);
+
 	return 0;
+}
+
+void multiplicacao(int num)
+{
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			printf("%d\n", i * j);			
+		}
+		
+	}
+	
 }
