e20d5fb34f073c659be57300357f7ab22412f63d
Felipe Costa <costa.felipe52@gmail.com>
2020-03-07 16:05:37 -0300

    Exercicios da lista de algoritmos em C

--- Lista08_Felipe_deAssis/ex_res/ex_res_08/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_08/main.c
@@ -8,9 +8,46 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+int calculoMinutos(int horaInicio, int minutoInicio, int horaTermino, int minutoTermino);
+
 int main(int argc, char const *argv[])
 {
-	int horaInicio, minutoInicio, horaTermino, minutoTermino;
-	
+	int horaInicio, minutoInicio, horaTermino, minutoTermino, minutos;
+
+	printf("\nInforme a hora inicial e final do jogo: ");
+	printf("\nHora inicial: \n(horas : minutos)\n-> ");
+	scanf("%d %d", &horaInicio, &minutoInicio);
+
+	printf("\nHora final: \n(horas : minutos)\n-> ");
+	scanf("%d %d", &horaTermino, &minutoTermino);
+
+	printf("\nHora inical informada: %d:%dhrs", horaInicio, minutoInicio);
+	printf("\nHora final informada: %d:%dhrs", horaTermino, minutoTermino);
+
+	minutos = calculoMinutos(horaInicio, minutoInicio, horaTermino, minutoTermino);
+	printf("\n\nDuração do jogo em minutos %d", minutos);
+
 	return 0;
+}
+
+int calculoMinutos(int horaInicio, int minutoInicio, int horaTermino, int minutoTermino)
+{
+	int totalHora, totalMinuto, total;
+
+	if (minutoTermino < minutoInicio)
+	{
+		minutoTermino += 60;
+		horaTermino -= 1;
+	}
+	if (horaTermino < horaInicio)
+	{
+		horaTermino += 24;
+	}
+	
+	totalMinuto = minutoTermino - minutoInicio;
+	totalHora = horaTermino - horaInicio;
+	total = totalHora * 60 + totalMinuto;
+
+	return total;
+	
 }
