a814520fd3728ca91554bb70afd36f62e5c98207
Felipe Costa <costa.felipe52@gmail.com>
2020-03-07 17:24:02 -0300

    Exercicios da lista de algoritmos em C

--- Lista08_Felipe_deAssis/ex_res/ex_res_09/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_09/main.c
@@ -8,8 +8,37 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+int maiorMenor(int num1, int num2);
+
 int main(int argc, char const *argv[])
 {
+	int num1, num2;
+
+	printf("\nCalcula o maior e menor valor: \n");
+
+	printf("\nInforme o primeiro numero:\n-> ");
+	scanf("%d", &num1);
+	printf("\nInforme o segundo numero:\n-> ");
+	scanf("%d", &num2);	
+
+	maiorMenor(num1, num2);
 	
 	return 0;
+}
+
+int maiorMenor(int num1, int num2)
+{
+	if (num1 > num2)
+	{
+		printf("\nO %d é maior que %d.", num1, num2);
+	}
+	else if (num1 < num2)
+	{
+		printf("\nO %d é maior que %d.", num2, num1);
+	}
+	else
+	{
+		printf("\nOs numeros são iguais.");
+	}
+	
 }
