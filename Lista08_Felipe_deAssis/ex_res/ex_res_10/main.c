25037ec37f933efe0d802ad055f7b2200520a7a8
Felipe Costa <costa.felipe52@gmail.com>
2020-03-08 09:32:22 -0300

    Exercicios da lista de algoritmos em C

--- Lista08_Felipe_deAssis/ex_res/ex_res_10/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_10/main.c
@@ -8,8 +8,36 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+int sequencia(int num);
+
 int main(int argc, char const *argv[])
 {
+	int num, s;
+
+	printf("\nCalcula o valor de 's': \n\ts = 1 + 1/1! + 1/2! + 1/3! + ... + 1/n!");
+	printf("\n\nInforme o valor de paremetro: \n-> ");
+	scanf("%d", &num);
+
+	s = sequencia(num);
+	printf("\t%d", s);
 	
 	return 0;
+}
+
+int sequencia(int num) 
+{
+	int a, b, f, seq;
+
+	seq = 1;
+
+	for (int i = 0; i <= num; i++)
+	{
+		f = 1;
+		for (int j = 0; j <= a; j++)
+		{
+			f *= b;
+		}
+		seq = seq + 1 / f;
+	}
+	
 }
--- Lista08_Felipe_deAssis/ex_res/ex_res_11/main.c
+++ Lista08_Felipe_deAssis/ex_res/ex_res_11/main.c
@@ -8,8 +8,66 @@ Data:
 #include <stdlib.h>
 #include <string.h>
 
+#define tam1 5
+#define tam2 4
+
 int main(int argc, char const *argv[])
 {
+	char sexo[tam1], corOlhos[tam1], corCabelo[tam1];
+	int idade[tam1];
+
+	for (int i = 0; i < tam1; i++)
+	{
+		printf("\n%dº habitante: ");
+		for (int j = 0; j < tam2; j++)
+		{
+			printf("\nInforme os dados: ");
+			printf("\nSexo: \n(f - feminino, m - masculino)\n-> ");
+			setbuf(stdin, NULL);
+			gets(sexo[j]);
+
+			printf("\nCor dos olhos: \n(a - azuis, c - castanho)\n-> ");
+			setbuf(stdin, NULL);
+			gets(corOlhos[j]);
+
+			printf("\nCor dos cabelos: \n(l - louros, p - pretos, c - castanhos)\n-> ");
+			setbuf(stdin, NULL);
+			gets(corCabelo[j]);
+
+			printf("\nIdade: \n-> ");
+			scanf("%d", &idade[j]);
+		}
+		
+	}
+
+	pesquisa(sexo, corOlhos, corCabelo, idade);
 	
 	return 0;
+}
+
+int pesquisa(char sexo[tam1], char corOlhos[tam1], char corCabelo[tam1], int idade[tam1])
+{
+	printf("\nMedia de idade das pessoas com olhos castanhos e cabelos pretos: %.2f.", mediaIdadeOlhosCastanhosCabelosPretos(corOlhos, corCabelo, idade));
+}
+
+float mediaIdadeOlhosCastanhosCabelosPretos(char corOlhos[tam1], char corCabelo[tam1], int idade[tam1])
+{
+	int	cont = 0, mediaIdade = 0;
+
+	for (int i = 0; i < tam1; i++)
+	{
+		for (int j = 0; j < tam2; j++)
+		{
+			if (corOlhos[j] == 'c' && corCabelo[j] == 'p')
+			{
+				mediaIdade += idade[j];
+				cont++;
+			}
+			
+		}
+		
+	}
+
+	return mediaIdade / cont++;
+		
 }
